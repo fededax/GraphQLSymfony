cd3e4f21ae19c1473ffc2efe791de3218b3ce2df
fede <fzurita@acasalud.com.ar>
2020-04-18 18:09:13 -0300

    first commit

---
+++ src/Form/PruebaType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Prueba;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PruebaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Prueba::class,
+        ]);
+    }
+}
